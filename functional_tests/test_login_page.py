e5baf74613e4816db301268834cfd52e628e6bfa
Trey Hunner <trey@treyhunner.com>
2022-10-11 17:31:48 -0700

    Add test for successful login

--- functional_tests/test_login_page.py
+++ functional_tests/test_login_page.py
@@ -21,3 +21,17 @@ class LoginTest(SplinterTestCase):
         )
         assert self.browser.is_text_not_present("Logout")
         assert self.browser.is_text_present("Login")
+
+    def test_valid_login(self):
+        user = User.objects.create_user(
+            email="kathleen@grenier.com",
+            password="my secret password",
+        )
+        self.browser.visit(self.live_server_url)  # Visit homepage
+        self.browser.find_by_text("Login").click()  # Click Login
+        self.browser.fill("username", user.email)
+        self.browser.fill("password", "my secret password")
+        self.browser.find_by_css("button[type=submit]").click()
+        self.assertEqual(self.browser.url, "/")
+        assert self.browser.is_text_present("Logout")
+        assert self.browser.is_text_not_present("Login")
