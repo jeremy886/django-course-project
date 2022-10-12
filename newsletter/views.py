5a7732a0b4a16d38fbf7cd2067758d7fd2d43444
Trey Hunner <trey@treyhunner.com>
2022-10-11 17:31:48 -0700

    Add almost-functional newsletter page form & test

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from .forms import SubscriberForm
+from .models import Subscriber
 
 
 def newsletter(request):
-    return render(request, 'newsletter.html')
+    if request.method == "POST":
+        form = SubscriberForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+            email = form.cleaned_data["email"]
+            subscriber = Subscriber(name=name, email=email)
+            subscriber.save()
+            return redirect("/")
+    else:
+        form = SubscriberForm()
+    return render(request, "newsletter.html", {"form": form})
