e7a7c59e77e47ca7ab2e79b72e70a1a6069dfd3e
Trey Hunner <trey@treyhunner.com>
2022-10-11 17:31:48 -0700

    Start a form and endpoint to add a product to cart

--- carts/views.py
+++ carts/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from carts.models import Cart
+from products.models import Product
 
 
 class CartView(LoginRequiredMixin, ListView):
@@ -11,3 +12,16 @@ class CartView(LoginRequiredMixin, ListView):
     def get_queryset(self):
         cart, created = Cart.objects.get_or_create(user=self.request.user)
         return cart.items.all()
+
+
+class AddToCartView(LoginRequiredMixin, DetailView):
+    http_method_names = ["post"]
+    model = Product
+    pk_url_kwarg = "product_id"
+
+    def post(self, request, product_id):
+        product = self.get_object()
+        cart, created = Cart.objects.get_or_create(user=request.user)
+        # TODO Add product to cart OR update product quantity (if it's already in the cart)
+        # TODO show success message
+        # TODO redirect to user's shopping cart page
