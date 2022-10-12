ebf4ab330288557001d4f5a058fe9ec75416f446
Trey Hunner <trey@treyhunner.com>
2022-10-11 17:31:48 -0700

    Create products app and Product model

---
+++ products/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=254)
+    description = models.CharField(max_length=254)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
