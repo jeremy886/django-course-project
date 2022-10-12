ea2034b198bdfc32eb3ce6ae41fe05e13208d63d
Trey Hunner <trey@treyhunner.com>
2022-10-11 17:31:48 -0700

    Add friendly string representation for Product model

--- products/models.py
+++ products/models.py
@@ -5,3 +5,6 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.CharField(max_length=254)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
