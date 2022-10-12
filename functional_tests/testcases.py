1914da2ea6902bbd9c8dad8b135c5ea9da5c8837
Trey Hunner <trey@treyhunner.com>
2022-10-11 17:31:48 -0700

    Add functional tests for cart page

--- functional_tests/testcases.py
+++ functional_tests/testcases.py
@@ -16,6 +16,12 @@ class SplinterTestCase(StaticLiveServerTestCase):
     def reverse(self, url_name, *args, **kwargs):
         return reverse(url_name, args=args, kwargs=kwargs)
 
+    def login(self, email, password):
+        self.visit("users:login")
+        self.browser.fill("username", email)
+        self.browser.fill("password", password)
+        self.browser.find_by_css("button[type=submit]").click()
+
     def visit(self, url_name, *args, **kwargs):
         if url_name.startswith("/") and not args and not kwargs:
             url = url_name
