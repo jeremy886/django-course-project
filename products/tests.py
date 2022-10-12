8717a17e64fbe19f504965a8ac16842a67297479
Trey Hunner <trey@treyhunner.com>
2022-10-11 17:31:48 -0700

    Add test for Product model creation

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Product
+
+
+class ProductModelTests(TestCase):
+
+    """Tests for Product model."""
+
+    def test_creation(self):
+        """Create a save a valid Product."""
+        duck = Product(
+            name="duck",
+            description="Adorable rubber duck",
+            price=1,
+        )
+        duck.save()
+        self.assertEqual(duck.name, "duck")
+        self.assertEqual(duck.description, "Adorable rubber duck")
+        self.assertEqual(duck.price, 1)
