b5ed68fca33cafd5ff79816021c4a3c84bac193b
Trey Hunner <trey@treyhunner.com>
2022-10-11 17:31:48 -0700

    Use django-widget-tweaks to customize newsletter form

---
+++ my_store/context_processors.py
@@ -0,0 +1,2 @@
+def widget_tweaks_errors(request):
+    return {"WIDGET_ERROR_CLASS": "is-invalid"}
--- my_store/settings.py
+++ my_store/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'my_store.context_processors.widget_tweaks_errors',
             ],
         },
     },
