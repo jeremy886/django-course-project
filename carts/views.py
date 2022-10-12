d3d15976889ca0dd74964c906cb53f573c94990e
Trey Hunner <trey@treyhunner.com>
2022-10-11 17:31:48 -0700

    Require user login to visit /cart/

--- carts/views.py
+++ carts/views.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
 
 from carts.models import Cart
 
 
-class CartView(ListView):
+class CartView(LoginRequiredMixin, ListView):
     template_name = "cart.html"
     context_object_name = "items"
 
--- my_store/settings.py
+++ my_store/settings.py
@@ -105,6 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = 'users.User'
 
+LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 
 
