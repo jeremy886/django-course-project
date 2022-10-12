89b1edd2500d6a47cf0a7e5e7cf86ac2ff94478c
Trey Hunner <trey@treyhunner.com>
2022-10-11 17:31:48 -0700

    Show 404 for missing product instead of 500

--- products/views.py
+++ products/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Product
 
 
 def product_details(request, product_id):
-    product = Product.objects.get(pk=product_id)
+    product = get_object_or_404(Product, pk=product_id)
     return render(request, 'product_details.html', {
         'product': product,
     })
