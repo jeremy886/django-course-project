d2cd83a4b30e74468142b2fc2600836f564e34b3
Trey Hunner <trey@treyhunner.com>
2022-10-11 17:31:48 -0700

    Start newsletter page

--- my_store/urls.py
+++ my_store/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
 
+
+def newsletter(request):
+    return HttpResponse('Subscribe to our newsletter')
+
+
 urlpatterns = [
+    path('newsletter/', newsletter),
     path('admin/', admin.site.urls),
 ]
