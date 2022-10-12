84916725dc90615df07b2b8d3f25e7b6a4213f6d
Trey Hunner <trey@treyhunner.com>
2022-10-11 17:31:48 -0700

    Add Cart to admin with CartItem inlines

--- carts/admin.py
+++ carts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Cart, CartItem
 
-# Register your models here.
+
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    inlines = [CartItemInline]
