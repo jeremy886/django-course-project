4b33291e98fb570ae86c51de11e3300794a5eeba
Trey Hunner <trey@treyhunner.com>
2022-10-11 17:31:48 -0700

    Customize Product string representation

--- products/tests.py
+++ products/tests.py
@@ -18,3 +18,13 @@ class ProductModelTests(TestCase):
         self.assertEqual(duck.name, "duck")
         self.assertEqual(duck.description, "Adorable rubber duck")
         self.assertEqual(duck.price, 1)
+
+    def test_string_representation(self):
+        """Check product string representation."""
+        duck = Product(
+            name="duck",
+            description="Adorable rubber duck",
+            price=1,
+        )
+        duck.save()
+        self.assertEqual(str(duck), "duck")
